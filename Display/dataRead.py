bdc4d4e7814ef44cd1d93611c766944add492892
julesDesjardin <oranjules@hotmail.fr>
2023-12-27 21:11:45 +0100

    Improve Display

--- Display/dataRead.py
+++ Display/dataRead.py
@@ -1,6 +1,6 @@
 import os
 
-def readLoop(canvas,nameText,singleText,averageText):
+def readLoop(stopEvent, canvas,nameText,singleText,averageText):
     while True:
         if 'changePending.txt' in os.listdir('.'):
             with open('./data.txt','r') as dataFile:
@@ -12,3 +12,5 @@ def readLoop(canvas,nameText,singleText,averageText):
             canvas.itemconfig(singleText, text='PR Single : ' + dataList[1])
             canvas.itemconfig(averageText, text='PR average : ' + dataList[2])
             os.remove('./changePending.txt')
+        if stopEvent.is_set():
+            break
--- Display/main.py
+++ Display/main.py
@@ -4,25 +4,34 @@ import os, threading
 import dataRead
 
 root = tk.Tk()
+root.title('Stream Display')
 
 def changePending():
     files = os.listdir('.')
     return 'changePending.txt' in files
 
+def closeWindow(window, event):
+    event.set()
+    window.destroy()
+
 main = tk.Frame(root)
 main.pack()
 
 width = 500
-lineHeight = 200
+leftLimit = 80
+lineHeight = 30
 totalHeight = 3*lineHeight
 
-canvas = tk.Canvas(main, width=width, height=totalHeight, background='magenta')
-nameText = canvas.create_text(width/2, lineHeight/2, text = '', anchor="center", justify='center')
-singleText = canvas.create_text(width/2, lineHeight+lineHeight/2, text= 'PR Single')
-averageText = canvas.create_text(width/2, 2*lineHeight+lineHeight/2, text= 'PR Average')
+canvas = tk.Canvas(main, width=width, height=totalHeight+5, background='magenta')
+line = canvas.create_rectangle(50, 5, 60, totalHeight, fill='black')
+nameText = canvas.create_text(leftLimit, lineHeight/2, text = '', anchor='w')
+singleText = canvas.create_text(leftLimit, lineHeight+lineHeight/2, text= 'PR Single', anchor='w')
+averageText = canvas.create_text(leftLimit, 2*lineHeight+lineHeight/2, text= 'PR Average', anchor='w')
 canvas.pack()
 
-readThread = threading.Thread(target=dataRead.readLoop, name="readLoop", args=(canvas,nameText, singleText, averageText))
+stopEvent = threading.Event()
+readThread = threading.Thread(target=dataRead.readLoop, name="readLoop", args=(stopEvent, canvas, nameText, singleText, averageText))
 readThread.start()
 
-root.mainloop()        
+root.protocol("WM_DELETE_WINDOW", lambda:closeWindow(root, stopEvent))
+root.mainloop()

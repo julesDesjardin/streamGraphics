65376f04132e91bf37462d037574c49c4b93fcd0
julesDesjardin <oranjules@hotmail.fr>
2024-03-10 18:53:57 +0100

    Refactor Cards to add Settings and remove the need of a Secret completely

---
+++ Cards/CardsSettings.py
@@ -0,0 +1,91 @@
+import tkinter as tk
+import tkinter.messagebox
+import tkinter.filedialog
+from tkinter import ttk
+import json, queue, threading
+from gql import gql, Client
+from gql.transport.aiohttp import AIOHTTPTransport
+
+import sys
+sys.path.append('.')
+from Common import TelegramBot
+
+class CardsSettings:
+
+    BG_COLOR = '#F4ECE1'
+
+    def __init__(self,root):
+        self.root = root
+        self.botToken = ''
+        self.botChannelId = ''
+        self.bot = None
+        self.queueLeft = queue.Queue()
+        self.queueRight = queue.Queue()
+
+    def botCallback(self, message):
+        fullMessage = message.text.removeprefix('/cardData ')
+        camera = fullMessage[0]
+        data = fullMessage[2:]
+        if camera == '0':
+            self.queueLeft.put(data)
+        elif camera == '1':
+            self.queueRight.put(data)
+
+    def saveSettings(self):
+        saveFile = tkinter.filedialog.asksaveasfile(initialdir='./',filetypes=(("JSON Files", "*.json"), ("All Files", "*.*")), defaultextension='.json')
+        saveSettingsJson = {
+            'botToken' : self.botToken,
+            'botChannelId' : self.botChannelId
+        }
+        saveFile.write(json.dumps(saveSettingsJson, indent=4))
+    
+    def loadSettings(self):
+        loadFile = tkinter.filedialog.askopenfile(initialdir='./',filetypes=(("JSON Files", "*.json"), ("All Files", "*.*")), defaultextension='.json')
+        loadSettingsJson = json.loads(loadFile.read())
+
+        self.botToken = loadSettingsJson['botToken']
+        self.botChannelId = loadSettingsJson['botChannelId']
+        self.bot = TelegramBot.TelegramBot(self.botToken,self.botChannelId)
+        self.bot.setMessageHandler(['timeTowerEvent'], lambda message:self.botCallback(message, self.compId))
+        self.bot.startPolling()
+
+    def updateTelegramSettingsCloseButton(self,token,id,window):
+        self.botToken = token
+        self.botChannelId = id
+        self.bot = TelegramBot.TelegramBot(token,id)
+        self.bot.setMessageHandler(['cardData'], self.botCallback)
+        self.threadBot = threading.Thread(target=self.bot.startPolling)
+        self.threadBot.start()
+        window.destroy()
+
+    def updateTelegramSettings(self):
+        telegramWindow = tk.Toplevel(self.root)
+        telegramLabel = tk.Label(telegramWindow,text='Please enter Telegram settings')
+        telegramLabel.pack(pady=20)
+        tokenLabel = tk.Label(telegramWindow,text='TimeTower bot token')
+        tokenLabel.pack(pady=5)
+        tokenEntry = tk.Entry(telegramWindow,width=50)
+        tokenEntry.pack(pady=5)
+        idLabel = tk.Label(telegramWindow,text='Channel ID between bots')
+        idLabel.pack(pady=5)
+        idEntry = tk.Entry(telegramWindow,width=50)
+        idEntry.pack(pady=5)
+        telegramCloseButton = tk.Button(telegramWindow,text='Save Telegram Settings',command=lambda:self.updateTelegramSettingsCloseButton(tokenEntry.get(),idEntry.get(),telegramWindow))
+        telegramCloseButton.pack(pady=20)
+
+    def showFrame(self):
+        frame = tk.Frame(self.root, bg=self.BG_COLOR, highlightbackground='black',highlightthickness=1)
+        settingsLabel = tk.Label(frame,text='Settings',bg=self.BG_COLOR)
+        settingsLabel.grid(column=0,row=0)
+        telegramButton = tk.Button(frame,text='Change Telegram Settings',command=self.updateTelegramSettings)
+        telegramButton.grid(column=0,row=1)
+        saveButton = tk.Button(frame,text='Save Settings...',command=self.saveSettings)
+        saveButton.grid(column=0,row=2)
+        saveButton = tk.Button(frame,text='Load Settings...',command=self.loadSettings)
+        saveButton.grid(column=0,row=3)
+        frame.pack(side=tk.LEFT,fill=tk.BOTH)
+        frame.columnconfigure(0, pad=20)
+        frame.rowconfigure(0, pad=20)
+        frame.rowconfigure(1, pad=20)
+        frame.rowconfigure(2, pad=20)
+        frame.rowconfigure(3, pad=20)
--- Cards/main.py
+++ Cards/main.py
@@ -1,16 +1,41 @@
 import tkinter as tk
 from tkinter import ttk
-import threading, queue
-import dataRead
+import CardsSettings
 
-import sys
-sys.path.append('.')
-from Common import TelegramBot, Secrets
+##############################################################################
+# FUNCTIONS
+##############################################################################
 
-bot = TelegramBot.TelegramBot(Secrets.cardBotToken,Secrets.interfaceCardChannelId)
+def checkQueue(root, queue, canvas, text):
+    while True:
+        try:
+            data = queue.get(timeout=0.1)
+        except:
+            break
+        canvas.itemconfig(text, text=data)
+    root.after(1000, lambda:checkQueue(root, queue, canvas, text))
+
+def checkBothQueues(root, queueLeft, queueRight, canvasLeft, canvasRight, textLeft, textRight):
+    checkQueue(root, queueLeft, canvasLeft, textLeft)
+    checkQueue(root, queueRight, canvasRight, textRight)
+
+##############################################################################
+# ROOT
+##############################################################################
 
 root = tk.Tk()
 root.title('Stream Cards')
+
+##############################################################################
+# SETTINGS
+##############################################################################
+
+localSettings = CardsSettings.CardsSettings(root)
+localSettings.showFrame()
+
+##############################################################################
+# MAIN
+##############################################################################
 
 main = tk.Frame(root)
 main.pack()
@@ -23,22 +48,11 @@ canvasLeft = tk.Canvas(main, width=width, height=height, background='#ff00ff')
 canvasRight = tk.Canvas(main, width=width, height=height, background='#ff00ff')
 textLeft = canvasLeft.create_text(100, 100, font=f'Helvetica {fontSize}', text='Bonjour', anchor='nw')
 textRight = canvasRight.create_text(100, 100, font=f'Helvetica {fontSize}', text='Bonjour', anchor='nw')
-
-queueLeft = queue.Queue()
-queueRight = queue.Queue()
-
-bot.setMessageHandler(['cardData'], lambda message:dataRead.botCallback(message, queueLeft, queueRight))
-threadBot = threading.Thread(target=bot.startPolling)
-threadBot.start()
-
-def startButtonAction(button):
-    dataRead.checkBothQueues(root, queueLeft, queueRight, canvasLeft, canvasRight, textLeft, textRight)
-    button.pack_forget()
-
-startButton = tk.Button(main, text='Start', command=lambda:startButtonAction(startButton))
-startButton.pack(side=tk.TOP)
 canvasLeft.pack(side=tk.LEFT)
 canvasRight.pack(side=tk.LEFT)
 
+##############################################################################
+
+checkBothQueues(root, localSettings.queueLeft, localSettings.queueRight, canvasLeft, canvasRight, textLeft, textRight)
+
 root.mainloop()
-threadBot.join()
